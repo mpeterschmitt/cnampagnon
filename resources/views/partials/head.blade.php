52bbbac32d91e22ef12236ad4d407dde7a187f65
Charles LESECQ <22601105+clesecq@users.noreply.github.com>
2025-12-08 12:06:24 +0100

    csp upgrade insecure requests

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -10,5 +10,7 @@
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
+<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+
 @vite(['resources/css/app.css', 'resources/js/app.js'])
 @fluxAppearance
