d66f3b8b28a90dab2b0adac09cd190911de5480e
mpeterschmitt <unurled@unurled.me>
2025-12-10 11:48:05 +0100

    fix: uv launch command

--- app/Services/PdfImportService.php
+++ app/Services/PdfImportService.php
@@ -32,7 +32,7 @@ class PdfImportService
 
         try {
 
-            $result = Process::run(['uv', 'python', $scriptPath, '--output', $outputFilePath, $filePath]);
+            $result = Process::run(['uv', 'run', $scriptPath, '--output', $outputFilePath, $filePath]);
 
             if ($result->failed()) {
                 throw new \Exception('Python Error: '.$result->errorOutput());
